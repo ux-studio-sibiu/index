fcdfdadaf6af0e01e7d7c69b59998257d43400c5
Razvan Turcanu <razvan.turcanu@visma.com>
2025-09-26 22:44:51 +0300

    update text

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,14 @@ import styles from './page.module.scss';
 export default async function Home() {
 
   return (
-    <div className = {`${styles['namespace-container']} clearfix hide-while-still-loading`}>
+    <div className = {`${styles['namespace-container']} clearfix `}>
 
-       UX.STUDIO.SIBIU
+       {/* <div className="studio-name">UX.STUDIO.SIBIU</div>
+       <div className="studio-mission">CREATIVE WEBSITES</div>
+       <div className="studio-contact">ux.studio.sibiu@gmail.com</div> */}
+
+        <div className="animation"></div>
+
 
       </div>
   );
