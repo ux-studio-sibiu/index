fd3d25deb9ed4539f42f6eb162103c831a82806e
Razvan Turcanu <razvan.turcanu@visma.com>
2025-09-26 23:26:13 +0300

    added responsize

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,10 @@ export default async function Home() {
     <div className = {`${styles['namespace-container']} clearfix `}>
 
        {/* <div className="studio-name">UX.STUDIO.SIBIU</div>
-       <div className="studio-mission">CREATIVE WEBSITES</div>
+       
        <div className="studio-contact">ux.studio.sibiu@gmail.com</div> */}
 
+        <div className="studio-mission">CREATIVE WEBSITES</div>
         <div className="animation"></div>
 
 
